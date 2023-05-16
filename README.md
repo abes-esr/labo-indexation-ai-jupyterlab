b89812f453302fd8f54fd2a0289a2cd5fbd070f3
Stéphane Gully <gully@abes.fr>
2023-05-16 15:48:22 +0200

    implementation des variables GIT_REPO1_URL GIT_REPO1_NAME et GIT_REPO2_URL GIT_REPO2_NAME

--- README.md
+++ README.md
@@ -16,4 +16,17 @@ docker run --rm -p 8888:8888 \
    -e JUPYTER_TOKEN=secret \
    --name labo-container-test \
    labo-image-test
+
+
+docker run --rm -p 8888:8888 \
+   -e GIT_REPO1_NAME=labo-indexation-ai \
+   -e GIT_REPO1_URL=https://github.com/abes-esr/labo-indexation-ai.git \
+   -e GIT_REPO2_NAME=abes-labo-movies \
+   -e GIT_REPO2_URL=https://github.com/abes-esr/abes-labo-movies.git \
+   -e JUPYTER_ENABLE_LAB=yes \
+   -e JUPYTER_TOKEN=secret \
+   --name labo-container-test \
+   labo-image-test
+
+
 ```
--- docker-entrypoint.overload.sh
+++ docker-entrypoint.overload.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 
-# Git clone/pull stuff
-if [ "$GIT_REPO" != "" ]; then
-  if [ ! -d /home/jovyan/work/.git/ ]; then
-    echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Git clone du dépot $GIT_REPO pour donner à manger data/ipynb/* à JupyterLab"
-    rm -rf /home/jovyan/work/
+# Clone des dépôts git
+if [ "$GIT_REPO1_NAME" != "" ]; then
+  if [ ! -d /home/jovyan/work/$GIT_REPO1_NAME/.git/ ]; then
+    echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Git clone du dépot $GIT_REPO1_NAME pour le JupyterLab"
+    rm -rf /home/jovyan/work/$GIT_REPO1_NAME/
     git config --global pull.rebase false
-    git clone "$GIT_REPO" /home/jovyan/work/
+    git clone "$GIT_REPO1_URL" /home/jovyan/work/$GIT_REPO1_NAME/
   else
-    echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Git clone du dépot $GIT_REPO déjà réalisé dans /home/jovyan/work/"
+    echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Git clone du dépot $GIT_REPO1_URL déjà réalisé dans /home/jovyan/work/$GIT_REPO1_NAME"
   fi
 else
-  echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Erreur: GIT_REPO doit être renseigné pour pouvoir travailler avec des fichiers ipynb collaborativement"
-  exit 1
+  echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Erreur: GIT_REPO1_URL GIT_REPO1_NAME doivent être renseignées pour pouvoir travailler avec des fichiers ipynb collaborativement"
 fi
+if [ "$GIT_REPO2_NAME" != "" ]; then
+  if [ ! -d /home/jovyan/work/$GIT_REPO2_NAME/.git/ ]; then
+    echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Git clone du dépot $GIT_REPO2_NAME pour le JupyterLab"
+    rm -rf /home/jovyan/work/$GIT_REPO2_NAME/
+    git config --global pull.rebase false
+    git clone "$GIT_REPO2_URL" /home/jovyan/work/$GIT_REPO2_NAME/
+  else
+    echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Git clone du dépot $GIT_REPO2_URL déjà réalisé dans /home/jovyan/work/$GIT_REPO2_NAME"
+  fi
+else
+  echo "--> $(date '+%Y-%m-%d %H:%M:%S') - Erreur: GIT_REPO2_URL GIT_REPO2_NAME doivent être renseignées pour pouvoir travailler avec des fichiers ipynb collaborativement"
+fi
+
+
 
 # start the real entrypoint
 # see https://github.com/jupyter/docker-stacks/blob/master/base-notebook/Dockerfile#L147
